af74313f5f714093dbe740029409e91be9981e61
Carlyson Viana <carlysonviana@MacBook-Pro-de-Carlyson.local>
2023-05-26 09:18:45 -0300

    Adicionar diretorio spring para projeto de consultas

---
+++ projeto/clinicalotus/src/main/java/com/saper/clinicalotus/ClinicalotusApplication.java
@@ -0,0 +1,13 @@
+package com.saper.clinicalotus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalotusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicalotusApplication.class, args);
+	}
+
+}
